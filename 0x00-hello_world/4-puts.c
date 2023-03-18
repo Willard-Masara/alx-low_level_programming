aa0f020bdd1dc9065f52d6873f8fecdc063d18e5
Willard-Masara <willardmasarat@gmail.com>
2023-03-17 20:11:18 -0700

    Betty debugger

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -THis prints a line without using ther tradityional printf
+ *Return: 0 (END OF PROGRAM)
+ */
+int main(void);
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
