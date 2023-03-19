a2aa1991042286b54e07227d325311ca440efbb0
Willard-Masara <willardmasarat@gmail.com>
2023-03-18 23:58:39 -0700

    Betty bettty

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *Main-A program to print words
+ *Return:0(End of programme)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
