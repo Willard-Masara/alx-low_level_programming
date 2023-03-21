ba8554a6226408dc1e1ac17afea0da91eeb3589f
Willard-Masara <willardmasarat@gmail.com>
2023-03-20 18:18:14 -0700

    Betyy and gcc

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-this is the betty function
+ *
+ * return:a zerofor this case
+ */
+int main (void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++);
+
+		putchar(1);
+	putchar("\n");
+	return (0);
+}
