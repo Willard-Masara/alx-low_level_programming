c5a6eea06759c5138ba19e79b9c0c4240e226c9a
Willard-Masara <willardmasarat@gmail.com>
2023-03-20 18:56:45 -0700

    Daunting betty

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main- main function on betty
+ *
+ *return: will be 0
+ */
+int main (void)
+ 
+{
+	for (i = 'a'; i <= 'z'; i++);
+ 	
+		putchar(1);
+	for (i = 'A'; i <= 'Z'; i++);
+ 	
+		putchar(i);
+	putchar(i);
+	return (0);
+}
