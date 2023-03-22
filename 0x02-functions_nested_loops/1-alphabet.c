ed2900891afbf369710b444cfe497e3e30412083
Willard-Masara <willardmasarat@gmail.com>
2023-03-22 00:45:22 -0700

    Loop problems

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * Main-entry point
+ * Description: a fo lopp
+ * Return:nothing
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	_putchar("\n");
+
+}
