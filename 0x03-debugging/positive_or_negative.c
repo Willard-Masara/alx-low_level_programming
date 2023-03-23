4065e260e7bed6b42ceff88cee42673ef9219a92
Willard-Masara <willardmasarat@gmail.com>
2023-03-23 16:40:08 -0700

    DEBUGGIND

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main-entry ppoint
+ * description-positve or negative output
+ * return: always 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf(" % d is negative\n", i);
+	else
+		printf(" % d is positive\n", i);
+
+}
+
