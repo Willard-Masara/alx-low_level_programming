bd07ca761c519e0bd3f51ddfb959ba5c1857ba58
Willard-Masara <willardmasarat@gmail.com>
2023-03-27 16:07:20 -0700

    Battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-enrty point
+ * description-this is a pointer
+ * return:always 0
+ */
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main-entry point
+ * description-main header file for pointer
+ * return: always 0
+ */
+void reset_to_98(int *n);
+#endif
