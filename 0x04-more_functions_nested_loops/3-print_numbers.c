e80c5b02b48bf529763707a0fec6654e0fc89087
Willard-Masara <willardmasarat@gmail.com>
2023-04-03 23:16:05 -0700

    Am back

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *@n-the number of values
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+	putchar(n);
+	}
+	putchar(10);
+
+
+}
