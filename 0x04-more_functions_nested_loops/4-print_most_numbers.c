697f21460bb836ab274f0611c652bc6e31cabee2
Willard-Masara <willardmasarat@gmail.com>
2023-04-03 23:33:52 -0700

    Savage

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *@n-numbers nu
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+	if ((n == 50) || (n == 52))
+	continue;
+	}
+	{
+	putchar(n);
+	}
+	putchar(10);
+
+}
