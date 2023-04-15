924914d7a8b35e0bb93e26df3a7b3bf2e813e5de
Willard-Masara <willardmasarat@gmail.com>
2023-04-14 22:20:37 -0700

    Busy week

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+*_memset-fills the memory with a constant byte
+*@s: memrory to be filled
+*@b: char to copy
+*@n: number of copy times
+*
+*return: pointer to s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+	return (s);
+
+}
