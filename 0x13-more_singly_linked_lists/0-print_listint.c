8985c0f54af9f320cab6c082aa3a542d6dc60a06
Willard-Masara <willardmasarat@gmail.com>
2023-05-02 11:53:43 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+*print_listint-to print all data of listint list
+*@h:head of list_t list.
+*
+*Return: n(nodes(list_t)
+*/
+size_t print_listint(const listint_t *h)
+
+{
+	size_t cells = 0;
+
+	while (h)
+	{
+	cells++;
+	printf("%d\n", h->nc);
+	h = h->next;
+
+
+	}
+	return (cells);
+
+}
