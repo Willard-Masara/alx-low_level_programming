be1fdd9cc892f8ba05d2594db768ad6a286be761
Willard-Masara <willardmasarat@gmail.com>
2023-06-19 09:40:57 -0700

    this is for the preprocessor project

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main-entry point to the function
+*description: prints the name of the file
+*return: always 0
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+
+}
