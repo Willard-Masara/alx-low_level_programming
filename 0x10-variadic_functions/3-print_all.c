5e03760a4ac2ea14b7fe1db196506ef7b50f55ca
Willard-Masara <willardmasarat@gmail.com>
2023-06-23 01:58:29 -0700

    fixing print all code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -38,7 +38,7 @@ void print_all(const char * const format, ...)
 
 	if (!str)
 	str = "(nil)";
-	printf(%s%s", pos, str);
+	printf("%s%s", pos, str);
 	break;
 	default:
 	i++;
@@ -51,7 +51,8 @@ void print_all(const char * const format, ...)
 
 
 }
-
+printf("\n");
+va_end(list);
 
 
 }
