efd997e37b4e3fd2d65eae8b727e71be155dc9bf
Willard-Masara <willardmasarat@gmail.com>
2023-08-21 08:52:47 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main-entry poit
+ *_isupper- checs for uppercase
+ *return:0 or 1
+ */
+int _isupper(int c)
+{
+	if ((c = 'A') && (c <= 'Z'))
+	{
+	return (1);
+	}
+	else
+	return (0);
+
+
+}
